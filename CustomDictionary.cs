bd4426a45b83f940aa4893ecde13b869fc901201
Aleksa <aleksa.tedi.markovic@gmail.com>
2025-10-15 18:26:52 +0200

    Initial commit
    Added gitignore
    Custom Dictionary implementation
    Some basic test cases

---
+++ CustomDictionary.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+
+namespace CustomDictionaryImplementation
+{
+    internal class CustomDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        private const int DefaultCapacity = 16;
+        private const float MaxLoadFactor = 0.75f;
+
+        private LinkedList<KeyValuePair<TKey, TValue>>[] _buckets;
+        private int _count;
+        private readonly IEqualityComparer<TKey> _comparer;
+
+        public int Count => _count;
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public CustomDictionary(int capacity = DefaultCapacity, IEqualityComparer<TKey> comparer = null)
+        {
+            if (capacity <= 0)
+            {
+                capacity = DefaultCapacity;
+            }
+
+            _buckets = new LinkedList<KeyValuePair<TKey, TValue>>[capacity];
+            _comparer = comparer ?? EqualityComparer<TKey>.Default;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!TryAdd(key, value))
+            {
+                throw new ArgumentException("An element with the same key already exists.");
+            }
+        }
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            EnsureTheresCapacityBeforeInsert();
+            int idx = GetBucketIndex(key);
+
+            LinkedList<KeyValuePair<TKey, TValue>> bucket = _buckets[idx];
+
+            if (bucket == null)
+            {
+                bucket = new LinkedList<KeyValuePair<TKey, TValue>>();
+                _buckets[idx] = bucket;
+            }
+
+            for (var node = bucket.First; node != null; node = node.Next)
+            {
+                if (_comparer.Equals(node.Value.Key, key))
+                {
+                    return false; // Key already exists
+                }
+            }
+
+            bucket.AddLast(new KeyValuePair<TKey, TValue>(key, value));
+            _count++;
+
+            return true;
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            int idx = GetBucketIndex(key);
+            var bucket = _buckets[idx];
+
+            if (bucket == null)
+            {
+                return false;
+            }
+
+            for (var node = bucket.First; node != null; node = node.Next)
+            {
+                if (_comparer.Equals(node.Value.Key, key))
+                {
+                    bucket.Remove(node);
+                    _count--;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            value = default;
+
+            int idx = GetBucketIndex(key);
+            var bucket = _buckets[idx];
+
+            if (bucket != null)
+            {
+                for (var node = bucket.First; node != null; node = node.Next)
+                {
+                    if (_comparer.Equals(node.Value.Key, key))
+                    {
+                        value = node.Value.Value;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private int GetBucketIndex(TKey key)
+        {
+            int hash = Math.Abs(_comparer.GetHashCode(key));
+            return hash % _buckets.Length;
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (TryGetValue(key, out var value))
+            {
+                return value;
+            }
+
+            throw new KeyNotFoundException("Key not found.");
+        }
+
+        private void EnsureTheresCapacityBeforeInsert()
+        {
+            if ((_count + 1) > _buckets.Length * MaxLoadFactor)
+            {
+                Resize(_buckets.Length * 2);
+            }
+        }
+
+        private void Resize(int newSize)
+        {
+            var newBuckets = new LinkedList<KeyValuePair<TKey, TValue>>[newSize];
+
+            foreach (var pair in this)
+            {
+                int hash = Math.Abs(_comparer.GetHashCode(pair.Key));
+                int idx = hash % newSize;
+
+                if (newBuckets[idx] == null)
+                {
+                    newBuckets[idx] = new LinkedList<KeyValuePair<TKey, TValue>>();
+                }
+
+                newBuckets[idx].AddLast(pair);
+            }
+            _buckets = newBuckets;
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            foreach (var bucket in _buckets)
+            {
+                if (bucket == null)
+                {
+                    continue;
+                }
+
+                foreach (var kv in bucket)
+                {
+                    yield return kv;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿namespace CustomDictionaryImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new CustomDictionary<string, int>();
+
+            AddTest(map);
+
+            LookupTest(map);
+
+            // Count
+            Console.WriteLine("Count: " + map.Count);
+
+            IterateTest(map);
+
+            RemoveElementTest(map);
+        }
+
+        private static void RemoveElementTest(CustomDictionary<string, int> map)
+        {
+            bool removed = map.Remove("two");
+            Console.WriteLine("Removed two: " + removed);
+            Console.WriteLine("Count after remove: " + map.Count);
+        }
+
+        private static void IterateTest(CustomDictionary<string, int> map)
+        {
+            foreach (var kv in map)
+            {
+                Console.WriteLine($"{kv.Key} -> {kv.Value}");
+            }
+        }
+
+        private static void LookupTest(CustomDictionary<string, int> map)
+        {
+            Console.WriteLine("two = " + map.Get("two")); // 22
+            try
+            {
+
+                Console.WriteLine("sixteen = " + map.Get("sixteen")); // will throw
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (map.TryGetValue("four", out var val))
+            {
+                Console.WriteLine("four: " + val);
+            }
+            else
+            {
+                Console.WriteLine("four not present");
+            }
+        }
+
+        private static void AddTest(CustomDictionary<string, int> map)
+        {
+            // Add
+            map.Add("one", 1);
+            map.Add("two", 2);
+
+            try
+            {
+                map.Add("two", 22); // will throw
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("AddOrThrow error: " + ex.Message);
+            }
+
+            try
+            {
+                map.TryAdd("two", 200); // will throw
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("AddOrThrow error: " + ex.Message);
+            }
+
+            bool added = map.TryAdd("three", 3);
+            Console.WriteLine($"Added 'three': {added}"); // true
+        }
+    }
+}
